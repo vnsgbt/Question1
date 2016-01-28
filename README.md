05bd1ccbdebcd82c05bfa15cda845fc721c6aaee
steve <vnsgbt@me.com>
2016-01-27 21:07:09 -0600

    Add more README

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Question1
+
+By dropping a phone at any R-floor we shall have two outcomes:
+   
+   - (a) It is broken: start with lowest level (L) up to R we shall find the breaking point. 
+   - (b) It is not: consider the next upper floor of R as the new (L) and try (a)
+   
+Since the requirement is that the least possible-drops cannot exceed 12 (lest than 50% of 50), 
+let's consider the first 12 floors as the range for (a).
+   
+   - If it is (a), we are safe to find breaking point within the next 11 drops. 
+   - If it's not, let's apply (b) with the next (previous_range - 1) floors to ensure maximum drops of 12.
+    
+To build and run with javac from directory src/main/java :
+
+javac org/vnsgbt/infoplus/Question1.java
+
+java -cp . org.vnsgbt.infoplus.Question1 [breakingPoint]
+   
+ 
+   
+  
--- src/test/java/org/vnsgbt/infoplus/LeastPossibleDropsTest.java
+++ src/test/java/org/vnsgbt/infoplus/LeastPossibleDropsTest.java
@@ -1,7 +1,6 @@
 package org.vnsgbt.infoplus;
 
 
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -11,17 +10,17 @@ import org.testng.annotations.Test;
  * Created by dev on 1/22/16.
  *
  */
-public class Question1Test {
+public class LeastPossibleDropsTest {
 
     @Test(dataProvider = "Fifty-floors-array")
     public void TestBreakingPointProperty (Class clazz, Integer breakingPoint) {
-        Question1 testWithBreakingPoint = new Question1(breakingPoint);
+        LeastPossibleDrops testWithBreakingPoint = new LeastPossibleDrops(breakingPoint);
         Assert.assertEquals(testWithBreakingPoint.getBreakingPoint(),breakingPoint.intValue());
     }
 
     @Test(dataProvider = "Fifty-floors-array")
     public void TestLeastPossibleDrops (Class clazz, Integer breakingPointAndLPD) {
-        Question1 testWithBreakingPoint = new Question1(breakingPointAndLPD);
+        LeastPossibleDrops testWithBreakingPoint = new LeastPossibleDrops(breakingPointAndLPD);
         int result = testWithBreakingPoint.getLeastPossibleDrops(testWithBreakingPoint.startTest);
         System.out.println("Optimal drops: " + result);
         Assert.assertTrue(result <= 12);
